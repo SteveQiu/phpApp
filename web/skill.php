843582f1f4a6789b6cbaad956006e4ce481409e3
SteveQiu <qiuyuliang20042005@hotmail.com>
2015-01-09 09:25:31 -0800

    Edit skill page

--- web/skill.php
+++ web/skill.php
@@ -1,10 +1,11 @@
 <div style="margin-top: 80px">
-	<h3>Advance Skills</h3>
-	<p>C/C++, Java</p>
+	<h3>Programming Languages</h3>
+	<p>C/C++, Java,Javascript, PHP, Verilog, VHDL</p>
 	<br>
-	<h3>Intermediate Skills</h3>
-	<p>Javascript, PHP, Verilog, VHDL</p>
-	<br>
-	<h3>Familiar Skills</h3>
+	<h3>Familiar Languages</h3>
 	<p>Python, Ruby, C#</p>
+	<br>
+	<h3>Databases</h3>
+	<p>MySQL, MongoDB</p>
+	<br>
 </div>

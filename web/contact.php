6ce86387eeb24dfac03e0c451faf0b33500f42ee
SteveQiu <qiuyuliang20042005@hotmail.com>
2020-08-08 10:30:48 -0700

    remove email

--- web/contact.php
+++ web/contact.php
@@ -1,6 +1,6 @@
 <div style="margin-top: 150px;">
 	<p><h1>Contact By Email:</h1>
 	<br>
-	<h1 id="iEmail">steveqiu@outlook.com</h1>
+	<h1 id="iEmail">https://www.linkedin.com/in/qsteve/</h1>
 	</p>
 </div>
